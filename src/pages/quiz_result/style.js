85175fb4358425a1ddc8d7b131549ce87796d3ae
kartikeymx4 <86483859+kartikeymx4@users.noreply.github.com>
2023-08-25 13:12:58 +0530

    Create style.js

---
+++ src/pages/quiz_result/style.js
@@ -0,0 +1,21 @@
+import { styled } from "styled-components";
+
+const Wrapper=styled.section`
+
+.quiz-result {
+  text-align: center;
+
+  h2 {
+    font-size: 2rem;
+    color: #333;
+    margin-bottom: 20px;
+  }
+
+  p {
+    font-size: 1.5rem;
+    color: #007bff;
+  }
+}
+
+`
+export default Wrapper;
